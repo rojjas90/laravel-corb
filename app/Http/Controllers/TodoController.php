3541e9737d401a8b14031bf76853d19acd96fe35
Rojas <sajor90@gmail.com>
2016-11-10 21:21:17 -0600

    Se agrego funcion csrf_field() de laravel para evitar inyeccion de datos en la vista, y se imprime el request en Store del controlador

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -44,7 +44,7 @@ class TodoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return $request;
     }
 
     /**
--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -10,13 +10,24 @@
   <body>
 <div class="container">
     <div class="bs-example" data-example-id="basic-forms">
-         <form>
-              <div class="form-group"> <label for="exampleInputEmail1">Email address</label> <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email"> </div>
-              <div class="form-group"> <label for="exampleInputPassword1">Password</label> <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"> </div>
-              <div class="form-group"> <label for="exampleInputFile">File input</label> <input type="file" id="exampleInputFile">
-                   <p class="help-block">Example block-level help text here.</p>
+         <form action="/todo" method="post">
+           {{csrf_field()}}
+              <div class="form-group">
+                      <label for="">Email address</label>
+                      <input type="text" class="form-control" id="" name="name" placeholder="name">
               </div>
-              <div class="checkbox"> <label> <input type="checkbox"> Check me out </label> </div> <button type="submit" class="btn btn-default">Submit</button> </form>
+              <div class="form-group">
+                      <label for="">Password</label>
+                      <input type="text" class="form-control" id="" name="color" placeholder="color">
+              </div>
+              <div class="form-group">
+                      <label for="">Password</label>
+                      <input type="text" class="form-control" id="" name="priority" placeholder="priority">
+              </div>
+
+
+               <button type="submit" class="btn btn-default">Submit</button>
+             </form>
     </div>
     </div>
   </body>
