f5a6ad500e09266d89f87a3289adaf32cf351597
Rojas <sajor90@gmail.com>
2016-11-11 20:19:27 -0600

    Anotaciones: // en consola tinker, para que muestre lo que se realiza cada vez que se consulta a la bd  >>> DB::listen(function($query){ var_dump($query);})  >>> App\Models\Todo::Find(1); string(52) "select * from `todos` where `todos`.`id` = ? limit 1" => App\Models\Todo {#711      id: 1,      created_at: "0000-00-00 00:00:00",      updated_at: "0000-00-00 00:00:00",      name: "test",      color: "red",      priority: 0,    }

--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -3,10 +3,7 @@
 <h1>Detalles de: {{$todo->name}}</h1>
 <br>
 <div class="bs-example" data-example-id="basic-forms">
-     <form action="/todo/{{$todo->id}}" method="post">
-       {{csrf_field()}}
-       {{ method_field('PUT') }}
-          <div class="form-group">
+           <div class="form-group">
                   <label for="">name</label>
                   <input type="text" class="form-control" id="" name="name" placeholder="name" value={{$todo->name}}>
           </div>
@@ -18,9 +15,6 @@
                   <label for="">priority</label>
                   <input type="text" class="form-control" id="" name="priority" placeholder="priority" value={{$todo->priority}}>
           </div>
-
-
-           <button type="submit" class="btn btn-default">Submit</button>
-         </form>
 </div>
+
 @stop
