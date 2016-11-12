3cc98305b6c7cea2c4c2894f050e17d98d3c545d
Rojas <sajor90@gmail.com>
2016-11-11 20:10:42 -0600

    Se agrego layout al proyecto

---
+++ resources/views/shared/_layout.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Layout</title>
+  </head>
+  <body>
+    <div class="container">
+
+          <div class="jumbotron">
+            <h1>Todo</h1>
+          </div>
+
+          <div class="row marketing">
+
+
+            <div class="conteiner">
+              <div class="col-lg-12">
+                @yield('content')
+              </div>
+            </div>
+
+          </div>
+
+          <!-- <footer class="footer">
+            <p>© 2016 Company, Inc.</p>
+          </footer> -->
+
+          <footer style="margin-top: 50px;">
+                            <p class="pull-right"><a href="#">Back to top</a></p>
+                            <p>© 2016 Company, Inc. · <!-- <a href="#">Privacy</a> · <a href="#">Terms</a> --></p>
+                          </footer>
+
+<script type="text/javascript">
+@yield('scripts')
+</script>
+
+
+        </div>
+    </body>
+</html>
