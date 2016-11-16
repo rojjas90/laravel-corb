369c155277e7a7b5b977e1e4176792cb3be1e3ea
Rojas <sajor90@gmail.com>
2016-11-15 21:14:33 -0600

    Se recuperaron los valores cuando la validacion falla y se muestran los mensajes en la vista Create de TodoController

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -1,15 +1,27 @@
 @extends('shared._layout')
 @section('content')
+@if (count($errors->all()))
+  <div class="danger">
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>
+      @endforeach
+
+    </ul>
+  </div>
+@endif
+
+
     <div class="bs-example" data-example-id="basic-forms">
          <form action="/todo" method="post">
            {{csrf_field()}}
               <div class="form-group">
                       <label for="">Name</label>
-                      <input type="text" class="form-control" id="" name="name" placeholder="name">
+                      <input type="text" class="form-control" id="" name="name" placeholder="name" value="{{old('name')}}">
               </div>
               <div class="form-group">
                       <label for="">Description</label>
-                      <input type="text" class="form-control" id="" name="description" placeholder="Description">
+                      <input type="text" class="form-control" id="" name="description" placeholder="Description" value="{{old('description')}}">
               </div>
               <div class="form-group">
                       <label for="">Color</label>
